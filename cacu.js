53be2fd01bb776bbc3485b8c0d502c67b2ec841e
quanle28197 <86665723+quanle28197@users.noreply.github.com>
2022-01-04 17:26:02 +0700

    update

--- cacu.js
+++ cacu.js
@@ -4,21 +4,21 @@ function Addition() {
     let addition = input + input2;
     document.getElementById("Addition").innerText = "Result: " + addition;
 }
-function Addition() {
+function Subtraction() {
     let input = document.getElementById("numberInput").value;
     let input2 = document.getElementById("numberInput2").value;
-    let addition = input + input2;
-    document.getElementById("Addition").innerText = "Result: " + addition;
+    let subtraction = input - input2;
+    document.getElementById("subtraction").innerText = "Result: " + subtraction;
 }
-function Addition() {
+function Multiptication() {
     let input = document.getElementById("numberInput").value;
     let input2 = document.getElementById("numberInput2").value;
-    let addition = input + input2;
-    document.getElementById("Addition").innerText = "Result: " + addition;
+    let multiptication = input * input2;
+    document.getElementById("Multiptication").innerText = "Result: " + multiptication;
 }
-function Addition() {
+function Division() {
     let input = document.getElementById("numberInput").value;
     let input2 = document.getElementById("numberInput2").value;
-    let addition = input + input2;
-    document.getElementById("Addition").innerText = "Result: " + addition;
+    let division = input + input2;
+    document.getElementById("Division").innerText = "Result: " + division;
 }
