d2bc6d53981e060a4981b7b4e239c3d9543f6b89
quanle28197 <86665723+quanle28197@users.noreply.github.com>
2022-01-04 17:15:17 +0700

    addd

--- cacu.js
+++ cacu.js
@@ -4,3 +4,21 @@ function Addition() {
     let addition = input + input2;
     document.getElementById("Addition").innerText = "Result: " + addition;
 }
+function Addition() {
+    let input = document.getElementById("numberInput").value;
+    let input2 = document.getElementById("numberInput2").value;
+    let addition = input + input2;
+    document.getElementById("Addition").innerText = "Result: " + addition;
+}
+function Addition() {
+    let input = document.getElementById("numberInput").value;
+    let input2 = document.getElementById("numberInput2").value;
+    let addition = input + input2;
+    document.getElementById("Addition").innerText = "Result: " + addition;
+}
+function Addition() {
+    let input = document.getElementById("numberInput").value;
+    let input2 = document.getElementById("numberInput2").value;
+    let addition = input + input2;
+    document.getElementById("Addition").innerText = "Result: " + addition;
+}
