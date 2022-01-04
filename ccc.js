57048b1d8b8984e569ac5471bb6b67ba1a515981
quanle28197 <86665723+quanle28197@users.noreply.github.com>
2022-01-04 17:10:59 +0700

    bosung

--- ccc.js
+++
@@ -1,7 +0,0 @@
-function moneyConvert() {
-    let input = +document.getElementById("moneyInput").value;
-    let source = +document.getElementById("sourceUnit").value;
-    let destination = +document.getElementById("destUnit").value;
-    let convert = input * destination / source;
-    document.getElementById("moneyConvert").innerText = "Result: " + convert;
-}
